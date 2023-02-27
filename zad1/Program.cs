338fba57be6ef5e2b2d3468ec926f7f6df7ae5bd
Lodre6123 <nikpash6123@mail.ru>
2023-02-27 16:05:27 +0300

    ggg

---
+++ zad1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Write("Кол-во строк равно: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Кол-во столбцов равно: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m,n];
+FillArrayRandomNumbers(array);
+PrintArray(array);
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
